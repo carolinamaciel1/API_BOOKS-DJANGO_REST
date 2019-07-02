053ee84763c1bc11a842777a39c02ed3eefb2dd2
carolinamaciel2 <carolinamaciel37@gmail.com>
2019-07-02 02:12:26 -0300

    Second commit: Creation of Book,CopyBook and RentBook at database.

--- book/models.py
+++ book/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
-# Create your models here.
+"""
+    Book: all atributtes of book model
+"""
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    author = models.CharField(max_length=200, null=False, blank=False)
+    publishing_company = models.CharField(max_length=150, null=True, blank=True)
+    registration_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+"""
+    CopyBook: all atributtes of copy book model
+"""
+
+
+class CopyBook(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=False, blank=False)
+    date_register = models.DateField()
+
+    def __str__(self):
+        return self.book
+
+
+"""
+    RentBook: all atributtes of rent book model
+"""
+
+
+class RentBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    book = models.ForeignKey(CopyBook, on_delete=models.CASCADE, null=False, blank=False)
+    date_initial_rent = models.DateField()
+    delivery_date_forecast = models.DateField()
+    date_devolution = models.DateField()
+
+    def __str__(self):
+        return self.book
